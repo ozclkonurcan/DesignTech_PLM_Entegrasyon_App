87613a953bdf541360a987832e833beb414d97ce
Onur Özçelik <71335949+ozclkonurcan@users.noreply.github.com>
2023-11-03 17:35:52 +0300

    03.11.2023 - 17:26

---
+++ DesignTech_PLM_Entegrasyon_App.MVC/Models/Log/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignTech_PLM_Entegrasyon_App.MVC.Models.Log
+{
+   public class LogEntry
+{
+        public string ExcelAdi { get; set; }
+        public string Hata { get; set; }
+    public int Satir { get; set; }
+    public int Sutun { get; set; }
+    public bool Durum { get; set; }
+        public DateTime Tarih { get; set; } = DateTime.Now;
+    }
+}
