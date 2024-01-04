6005e4dee0e65b78e54a50db5928659ae03d9ead
Onur Özçelik <71335949+ozclkonurcan@users.noreply.github.com>
2024-01-04 16:55:45 +0300

    04.01.2024 - 16:55

--- DesignTech_PLM_Entegrasyon_App.MVC/Controllers/Modules/WindowsFormSettings/WindowsFormSettingsController.cs
+++ DesignTech_PLM_Entegrasyon_App.MVC/Controllers/Modules/WindowsFormSettings/WindowsFormSettingsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DesignTech_PLM_Entegrasyon_App.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 
 namespace DesignTech_PLM_Entegrasyon_App.MVC.Controllers.Modules.WindowsFormSettings
@@ -26,10 +27,31 @@ namespace DesignTech_PLM_Entegrasyon_App.MVC.Controllers.Modules.WindowsFormSett
 
                 // Dosya yolu ve dosya adını al
                 var windowsFormFileUrl = jsonObj["WindowsFormFileUrl"]?.ToString();
+                var windowsFormFileUrl2 = jsonObj["ApiSendDataSettingsFolder"]?.ToString();
 
             
                     var targetJson = System.IO.File.ReadAllText(windowsFormFileUrl);
+                    var targetJson2 = System.IO.File.ReadAllText(windowsFormFileUrl2);
                     var targetJsonObj = JObject.Parse(targetJson);
+                    var targetJsonObj2 = JObject.Parse(targetJson2);
+
+
+
+                var wtPartMasterList = new List<WTPartMasterItemViewModel>();
+
+                foreach (var item in targetJsonObj2["WTPartMaster"])
+                {
+                    var wtPartMasterItem = new WTPartMasterItemViewModel
+                    {
+                        ID = item.Value<string>("ID"),
+                        Name = item.Value<string>("Name"),
+                        SQLName = item.Value<string>("SQLName"),
+                        IsActive = item.Value<bool>("IsActive")
+                    };
+
+                    wtPartMasterList.Add(wtPartMasterItem);
+                }
+
 
                 // Değişiklikleri yap
                 ViewBag.Catalog = targetJsonObj["Catalog"].ToString();
@@ -40,6 +62,8 @@ namespace DesignTech_PLM_Entegrasyon_App.MVC.Controllers.Modules.WindowsFormSett
                 ViewBag.ApiEndpoint = targetJsonObj["APIConnectionINFO"]["ApiEndpoint"].ToString();
                 ViewBag.API = targetJsonObj["APIConnectionINFO"]["API"].ToString();
                 ViewBag.ConnectionType = Convert.ToBoolean(targetJsonObj["ConnectionType"]);
+
+                ViewBag.ApiSendDataSettings = wtPartMasterList;
 
                     // View'e geçir
                     ViewBag.TargetJson = targetJsonObj.ToString();
@@ -54,7 +78,49 @@ namespace DesignTech_PLM_Entegrasyon_App.MVC.Controllers.Modules.WindowsFormSett
         }
 
 
-        public IActionResult windowsFormURLSettings(string jsonFile)
+        public IActionResult ApiSendDataSettings(string jsonFile)
+        {
+            try
+            {
+                if (jsonFile != null && jsonFile.Length > 0)
+                {
+                 
+                    // appsettings.json dosyasını oku
+                    var appSettingsPath = "appsettings.json";
+                    var json = System.IO.File.ReadAllText(appSettingsPath);
+                    var jsonObj = JObject.Parse(json);
+
+                    // Eğer WindowsFormFileUrl zaten varsa güncelle, yoksa ekle
+                    if (jsonObj["ApiSendDataSettingsFolder"] == null)
+                    {
+                        jsonObj.Add("ApiSendDataSettingsFolder", jsonFile);
+                    }
+                    else
+                    {
+                        jsonObj["ApiSendDataSettingsFolder"] = jsonFile;
+                    }
+
+                    // Değişiklikleri kaydet
+                    System.IO.File.WriteAllText(appSettingsPath, jsonObj.ToString());
+
+                    // Başarılı mesajı
+                    TempData["SuccessMessage"] = "ApiSendDataSettings.json dosyası güncellendi. Dosya Yolu: " + jsonFile;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Hata! Dosya seçimi yapılmadı.";
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Hata! " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    
+      public IActionResult windowsFormURLSettings(string jsonFile)
         {
             try
             {
---
+++ DesignTech_PLM_Entegrasyon_App.MVC/ViewModels/WTPartMasterItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesignTech_PLM_Entegrasyon_App.MVC.ViewModels
+{
+    public class WTPartMasterItemViewModel
+    {
+        [Key]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string SQLName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
