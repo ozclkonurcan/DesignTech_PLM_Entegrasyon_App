7ff5fb71451e161b8f054e38e10922507485ef9c
Onur Özçelik <71335949+ozclkonurcan@users.noreply.github.com>
2024-03-26 11:05:28 +0300

    26.03.2024 11:05

--- DesignTech_PLM_Entegrasyon_App.MVC/Hubs/FormDataListHub.cs
+++ DesignTech_PLM_Entegrasyon_App.MVC/Hubs/FormDataListHub.cs
@@ -11,172 +11,193 @@ namespace DesignTech_PLM_Entegrasyon_App.MVC.Hubs
 {
     public class FormDataListHub : Hub
     {
-		private readonly IConfiguration _configuration;
-		private readonly IHttpClientFactory _httpClientFactory;
-		private readonly IGenericRepository<Change_Notice_LogTable> _change_Notice_LogTable;
-		public FormDataListHub(IHttpClientFactory httpClientFactory, IConfiguration configuration, IGenericRepository<Change_Notice_LogTable> change_Notice_LogTable = null)
-		{
-			_httpClientFactory = httpClientFactory;
-			_configuration = configuration;
-			_change_Notice_LogTable = change_Notice_LogTable;
-		}
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IGenericRepository<Change_Notice_LogTable> _change_Notice_LogTable;
+        private readonly IGenericRepository<WTPartAlternateLink_LOG> _wTPartAlternateLink_LOG;
+        public FormDataListHub(IHttpClientFactory httpClientFactory, IConfiguration configuration, IGenericRepository<Change_Notice_LogTable> change_Notice_LogTable = null, IGenericRepository<WTPartAlternateLink_LOG> wTPartAlternateLink_LOG = null)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+            _change_Notice_LogTable = change_Notice_LogTable;
+            _wTPartAlternateLink_LOG = wTPartAlternateLink_LOG;
+        }
 
-		public async Task SendFormDataCount()
-		{
-			try
-			{
-
-		
-			//string connectionString = _configuration.GetConnectionString("Plm");
-			//using IDbConnection connection = new SqlConnection(connectionString);
-			string schema = _configuration["Catalog"];
+        public async Task SendFormDataCount()
+        {
+            try
+            {
 
 
-			// Sorguları optimize edin ve sadece gerekli sütunları çekin
-			//var Change_Notice_LogTableList = connection.Query<Change_Notice_LogTable>($"SELECT * FROM {schema}.Change_Notice_LogTable").OrderByDescending(x => x.ProcessTimestamp).ToList();
-			var Change_Notice_LogTableList = (await _change_Notice_LogTable.GetAll(schema+ ".Change_Notice_LogTable")).OrderByDescending(x => x.ProcessTimestamp).ToList();
-
-			if (Change_Notice_LogTableList is not null)
-			{
-
-				
-
-					var value = Change_Notice_LogTableList;
-					var epmDocuments = value.Where(item => item.VersionID.Contains("EPMDocument")).ToList();
-					var wtParts = value.Where(item => item.VersionID.Contains("WTPart")).ToList();
-					if (epmDocuments.Any())
-					{
-						await Clients.All.SendAsync("EPMDocumentFormDataCount", epmDocuments.Count);
-					}
-					if (wtParts.Any())
-					{
-						await Clients.All.SendAsync("ReceiveFormDataCount", wtParts.Count);
-					}
-					//await Clients.All.SendAsync("ReceiveFormDataCount",value);
-				}
-			}
-			catch (Exception)
-			{
-
-			}
-		}
-
-		public async Task SendFormData()
-		{
-			try
-			{
-
-			
-			//string connectionString = _configuration.GetConnectionString("Plm");
-			//using IDbConnection connection = new SqlConnection(connectionString);
-			string schema = _configuration["Catalog"];
+                //string connectionString = _configuration.GetConnectionString("Plm");
+                //using IDbConnection connection = new SqlConnection(connectionString);
+                string schema = _configuration["Catalog"];
 
 
-				// Sorguları optimize edin ve sadece gerekli sütunları çekin
-				//var Change_Notice_LogTableList = connection.Query<Change_Notice_LogTable>($"SELECT * FROM {schema}.Change_Notice_LogTable").OrderByDescending(x => x.ProcessTimestamp).ToList();
-				var Change_Notice_LogTableList = (await _change_Notice_LogTable.GetAll(schema + ".Change_Notice_LogTable")).OrderByDescending(x => x.ProcessTimestamp).ToList();
-				Change_Notice_LogTableList.ForEach(log =>
-				{
-					log.ProcessTimestamp = Convert.ToDateTime(log.ProcessTimestamp).AddHours(3);
-				});
-				var durumTakip = false;
-			if (Change_Notice_LogTableList is not null)
-			{
-					var value = Change_Notice_LogTableList;
-					var epmDocuments = value.Where(item => item.VersionID.Contains("EPMDocument")).ToList();
-					var wtParts = value.Where(item => item.VersionID.Contains("WTPart")).ToList();
-					if (epmDocuments.Any())
-					{
-						await Clients.All.SendAsync("EPMDocumentFormData", epmDocuments);
-					}
-					if (wtParts.Any())
-					{
-						await Clients.All.SendAsync("ReceiveFormData", wtParts);
-					}
-				
-					
-				}
-			}
-			catch (Exception)
-			{
+                // Sorguları optimize edin ve sadece gerekli sütunları çekin
+                //var Change_Notice_LogTableList = connection.Query<Change_Notice_LogTable>($"SELECT * FROM {schema}.Change_Notice_LogTable").OrderByDescending(x => x.ProcessTimestamp).ToList();
+                var Change_Notice_LogTableList = (await _change_Notice_LogTable.GetAll(schema + ".Change_Notice_LogTable")).OrderByDescending(x => x.ProcessTimestamp).ToList();
+                var WTPartAlternateLink_LOGList = (await _change_Notice_LogTable.GetAll(schema + ".WTPartAlternateLink_LOG")).OrderByDescending(x => x.ProcessTimestamp).ToList();
 
-				throw;
-			}
-
-		}
-
-
-		public async Task SendWorkStatus()
-		{
-			try
-			{
-				var appSettingsPath = "appsettings.json";
-
-				// appsettings.json dosyasını oku
-				var json = System.IO.File.ReadAllText(appSettingsPath);
-				var jsonObj = JObject.Parse(json);
-				// Dosya yolu ve dosya adını al
-				var windowsFormFileUrl = jsonObj["WindowsFormFileUrl"]?.ToString();
-				var targetJson = System.IO.File.ReadAllText(windowsFormFileUrl);
-				var targetJsonObj = JObject.Parse(targetJson);
-
-
-				bool connectionType = Convert.ToBoolean(targetJsonObj["ConnectionType"]);
-				await Clients.All.SendAsync("ReceiveWorkStatus", connectionType);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
-		}
+                if (Change_Notice_LogTableList is not null)
+                {
 
 
 
-		// Scroll kaydırıldığında verileri göstermeye çalışacaz
-		 //public async Task SendInitialFormData(int pageSize)
-   // {
-   //     try
-   //     {
-   //         string schema = _configuration["Catalog"];
-   //         var initialData = (await _change_Notice_LogTable.GetPageData(schema + ".Change_Notice_LogTable", 1, pageSize)).OrderByDescending(x => x.ProcessTimestamp).ToList();
-
-   //         var formattedData = FormatData(initialData);
-   //         await Clients.All.SendAsync("ReceiveInitialFormData", formattedData);
-   //     }
-   //     catch (Exception ex)
-   //     {
-   //         Console.WriteLine(ex.Message);
-   //     }
-   // }
-
-   // public async Task SendAdditionalFormData(int pageNumber, int pageSize)
-   // {
-   //     try
-   //     {
-   //         string schema = _configuration["Catalog"];
-   //         var additionalData = (await _change_Notice_LogTable.GetPageData(schema + ".Change_Notice_LogTable", pageNumber, pageSize)).OrderByDescending(x => x.ProcessTimestamp).ToList();
-
-   //         var formattedData = FormatData(additionalData);
-   //         await Clients.All.SendAsync("ReceiveAdditionalFormData", formattedData);
-   //     }
-   //     catch (Exception ex)
-   //     {
-   //         Console.WriteLine(ex.Message);
-   //     }
-   // }
-
-   // private List<Change_Notice_LogTable> FormatData(List<Change_Notice_LogTable> data)
-   // {
-   //     // Verilerinizi uygun bir ViewModel'e dönüştürme işlemini burada gerçekleştirin.
-   //     // Örneğin, bu kodu kullanabilirsiniz:
-   //     // var formattedData = data.Select(item => new ChangeNoticeLogTableViewModel { ... }).ToList();
-   //     // return formattedData;
-
-   //     // Eğer ViewModel kullanmıyorsanız, direkt olarak data'yı gönderebilirsiniz.
-   //     return data;
-   // }
-		// Scroll kaydırıldığında verileri göstermeye çalışacaz
+                    var value = Change_Notice_LogTableList;
+                    var epmDocuments = value.Where(item => item.VersionID.Contains("EPMDocument")).ToList();
+                    var wtParts = value.Where(item => item.VersionID.Contains("WTPart")).ToList();
+                    if (epmDocuments.Any())
+                    {
+                        await Clients.All.SendAsync("EPMDocumentFormDataCount", epmDocuments.Count);
+                    }
+                    if (wtParts.Any())
+                    {
+                        await Clients.All.SendAsync("ReceiveFormDataCount", wtParts.Count);
+                    }
+                    if (WTPartAlternateLink_LOGList is not null)
+                    {
+                        if (WTPartAlternateLink_LOGList.Any())
+                        {
+                            await Clients.All.SendAsync("AlternatePartsFormDataCount", WTPartAlternateLink_LOGList.Count);
+                        }
+                    }
 
 
-	}
+                    //await Clients.All.SendAsync("ReceiveFormDataCount",value);
+                }
+
+              
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public async Task SendFormData()
+        {
+            try
+            {
+
+
+                //string connectionString = _configuration.GetConnectionString("Plm");
+                //using IDbConnection connection = new SqlConnection(connectionString);
+                string schema = _configuration["Catalog"];
+
+
+                // Sorguları optimize edin ve sadece gerekli sütunları çekin
+                //var Change_Notice_LogTableList = connection.Query<Change_Notice_LogTable>($"SELECT * FROM {schema}.Change_Notice_LogTable").OrderByDescending(x => x.ProcessTimestamp).ToList();
+                var Change_Notice_LogTableList = (await _change_Notice_LogTable.GetAll(schema + ".Change_Notice_LogTable")).OrderByDescending(x => x.ProcessTimestamp).ToList();
+                var WTPartAlternateLink_LOGList = (await _wTPartAlternateLink_LOG.GetAll(schema + ".WTPartAlternateLink_LOG")).OrderByDescending(x => x.ProcessTimestamp).ToList();
+                Change_Notice_LogTableList.ForEach(log =>
+                {
+                    log.ProcessTimestamp = Convert.ToDateTime(log.ProcessTimestamp).AddHours(3);
+                });
+                var durumTakip = false;
+                if (Change_Notice_LogTableList is not null)
+                {
+                    var value = Change_Notice_LogTableList;
+                    var epmDocuments = value.Where(item => item.VersionID.Contains("EPMDocument")).ToList();
+                    var wtParts = value.Where(item => item.VersionID.Contains("WTPart")).ToList();
+                    if (epmDocuments.Any())
+                    {
+                        await Clients.All.SendAsync("EPMDocumentFormData", epmDocuments);
+                    }
+                    if (wtParts.Any())
+                    {
+                        await Clients.All.SendAsync("ReceiveFormData", wtParts);
+                    }
+
+                    if(WTPartAlternateLink_LOGList is not null)
+                    {
+                        await Clients.All.SendAsync("AlternatePartsFormData", WTPartAlternateLink_LOGList);
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        public async Task SendWorkStatus()
+        {
+            try
+            {
+                var appSettingsPath = "appsettings.json";
+
+                // appsettings.json dosyasını oku
+                var json = System.IO.File.ReadAllText(appSettingsPath);
+                var jsonObj = JObject.Parse(json);
+                // Dosya yolu ve dosya adını al
+                var windowsFormFileUrl = jsonObj["WindowsFormFileUrl"]?.ToString();
+                var targetJson = System.IO.File.ReadAllText(windowsFormFileUrl);
+                var targetJsonObj = JObject.Parse(targetJson);
+
+
+                bool connectionType = Convert.ToBoolean(targetJsonObj["ConnectionType"]);
+                await Clients.All.SendAsync("ReceiveWorkStatus", connectionType);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+        // Scroll kaydırıldığında verileri göstermeye çalışacaz
+        //public async Task SendInitialFormData(int pageSize)
+        // {
+        //     try
+        //     {
+        //         string schema = _configuration["Catalog"];
+        //         var initialData = (await _change_Notice_LogTable.GetPageData(schema + ".Change_Notice_LogTable", 1, pageSize)).OrderByDescending(x => x.ProcessTimestamp).ToList();
+
+        //         var formattedData = FormatData(initialData);
+        //         await Clients.All.SendAsync("ReceiveInitialFormData", formattedData);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine(ex.Message);
+        //     }
+        // }
+
+        // public async Task SendAdditionalFormData(int pageNumber, int pageSize)
+        // {
+        //     try
+        //     {
+        //         string schema = _configuration["Catalog"];
+        //         var additionalData = (await _change_Notice_LogTable.GetPageData(schema + ".Change_Notice_LogTable", pageNumber, pageSize)).OrderByDescending(x => x.ProcessTimestamp).ToList();
+
+        //         var formattedData = FormatData(additionalData);
+        //         await Clients.All.SendAsync("ReceiveAdditionalFormData", formattedData);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine(ex.Message);
+        //     }
+        // }
+
+        // private List<Change_Notice_LogTable> FormatData(List<Change_Notice_LogTable> data)
+        // {
+        //     // Verilerinizi uygun bir ViewModel'e dönüştürme işlemini burada gerçekleştirin.
+        //     // Örneğin, bu kodu kullanabilirsiniz:
+        //     // var formattedData = data.Select(item => new ChangeNoticeLogTableViewModel { ... }).ToList();
+        //     // return formattedData;
+
+        //     // Eğer ViewModel kullanmıyorsanız, direkt olarak data'yı gönderebilirsiniz.
+        //     return data;
+        // }
+        // Scroll kaydırıldığında verileri göstermeye çalışacaz
+
+
+    }
 }
---
+++ DesignTech_PLM_Entegrasyon_App.MVC/Models/LogTable/WTPartAlternateLink_LOG.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesignTech_PLM_Entegrasyon_App.MVC.Models.LogTable
+{
+    public class WTPartAlternateLink_LOG
+    {
+        public string? ID { get; set; }
+        public string? ObjectType { get; set; }
+        public string? Name { get; set; }
+        public string? Number { get; set; }
+        public string? State { get; set; }
+        public DateTime updateStampA2 { get; set; }
+        public DateTime modifyStampA2 { get; set; }
+        public DateTime ProcessTimestamp { get; set; }
+    }
+}
